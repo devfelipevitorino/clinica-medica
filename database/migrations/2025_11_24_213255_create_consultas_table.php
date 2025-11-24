7d07b78862ab23ad56e224dd6f17d09c3e926458
devfelipevitorino <devfelipevitorino@gmail.com>
2025-11-24 18:36:51 -0300

    Adicionar migration: create_consultas_table

---
+++ database/migrations/2025_11_24_213255_create_consultas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('paciente_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('medico_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('data_hora');
+            $table->string('motivo')->nullable();
+            $table->text('observacoes')->nullable();
+            $table->enum('status', ['agendada', 'realizada', 'cancelada'])->default('agendada');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultas');
+    }
+};
