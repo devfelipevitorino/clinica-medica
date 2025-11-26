118b121c813d5f086e2bd14d5a76e7648e9cfc4f
devfelipevitorino <devfelipevitorino@gmail.com>
2025-11-26 20:59:04 -0300

    feat: atualizar model Consulta com novos relacionamentos e regras
    - Adiciona novos relacionamentos necessários
    - Ajusta atributos fillable e casts

--- app/Models/Consulta.php
+++ app/Models/Consulta.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Consulta extends Model
 {
-    //
+    protected $fillable = [
+        'paciente_id',
+        'medico_id',
+        'data',
+        'hora',
+        'observacoes'
+    ];
+
+    public function paciente()
+    {
+        return $this->belongsTo(Paciente::class);
+    }
+
+    public function medico()
+    {
+        return $this->belongsTo(Medico::class);
+    }
 }
