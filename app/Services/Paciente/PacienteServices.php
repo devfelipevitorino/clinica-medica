914fb056623abfaee3f997533e0142e33c4a398e
devfelipevitorino <devfelipevitorino@gmail.com>
2025-12-08 20:33:23 -0300

    refactor: melhorias no PacienteService

--- app/Services/Paciente/PacienteServices.php
+++ app/Services/Paciente/PacienteServices.php
@@ -3,7 +3,6 @@
 namespace App\Services\Paciente;
 
 use App\Http\Repositories\Paciente\PacienteRepositories;
-use App\Models\Paciente;
 
 class PacienteServices
 {
@@ -18,5 +17,39 @@ class PacienteServices
     public function index()
     {
         return $this->repository->index();
+    }
+
+    public function store($request)
+    {
+        $paciente = $this->repository->storePaciente($request);
+
+        $this->repository->storeEndereco($paciente, $request);
+
+        return $paciente;
+    }
+
+    public function edit($id)
+    {
+        return $this->repository->findForEdit($id);
+    }
+
+    public function update($request, $id)
+    {
+        $paciente = $this->repository->findWithEndereco($id);
+
+        $this->repository->updatePaciente($paciente, $request);
+
+        $this->repository->updateEndereco($paciente, $request);
+
+        return $paciente;
+    }
+
+    public function destroy($id)
+    {
+        $paciente = $this->repository->findById($id);
+
+        $this->repository->destroy($paciente);
+
+        return true;
     }
 }
