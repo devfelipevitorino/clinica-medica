e9c21dded157ffd00b8f1d0577e1f0ce81ec57b1
devfelipevitorino <devfelipevitorino@gmail.com>
2025-12-08 20:33:18 -0300

    refactor: ajustes no PacienteRepository

--- app/Http/Repositories/Paciente/PacienteRepositories.php
+++ app/Http/Repositories/Paciente/PacienteRepositories.php
@@ -17,4 +17,53 @@ class PacienteRepositories
     {
         return $this->model->with('endereco')->get();
     }
+
+    public function findForEdit($id)
+    {
+        return $this->model->with('endereco')->findOrFail($id);
+    }
+
+    public function findWithEndereco($id)
+    {
+        return $this->model->with('endereco')->findOrFail($id);
+    }
+
+    public function findById($id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+
+    public function storePaciente($request)
+    {
+        return $this->model->create(
+            $request->only(['nome', 'cpf', 'email', 'telefone', 'data_nascimento'])
+        );
+    }
+
+    public function storeEndereco($paciente, $request)
+    {
+        $paciente->endereco()->create(
+            $request->only(['rua', 'numero', 'bairro', 'cidade', 'estado', 'cep'])
+        );
+    }
+
+    public function updatePaciente($paciente, $request)
+    {
+        $paciente->update(
+            $request->only(['nome', 'cpf', 'email', 'telefone', 'data_nascimento'])
+        );
+    }
+
+    public function updateEndereco($paciente, $request)
+    {
+        $paciente->endereco->update(
+            $request->only(['rua', 'numero', 'bairro', 'cidade', 'estado', 'cep'])
+        );
+    }
+
+    public function destroy($paciente)
+    {
+        $paciente->delete();
+    }
 }
