a7b194e3557e19e54f338e38cd6febdc01195f96
devfelipevitorino <devfelipevitorino@gmail.com>
2025-12-14 18:48:52 -0300

    feat: cria RelatorioController

---
+++ app/Http/Controllers/RelatorioController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Consulta\ConsultaServices;
+use App\Services\Paciente\PacienteServices;
+
+class RelatorioController extends Controller
+{
+    private $pacienteServices;
+    private $consultasServices;
+
+    public function __construct(PacienteServices $pacienteServices, ConsultaServices $consultaServices)
+    {
+        $this->pacienteServices = $pacienteServices;
+        $this->consultasServices = $consultaServices;
+    }
+
+    public function index()
+    {
+        $pacientes = $this->pacienteServices->index();
+        $consultas = $this->consultasServices->index();
+        return view('relatorios.index', compact('pacientes', 'consultas'));
+    }
+}
